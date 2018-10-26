dfb37c2f1dc0b884b9d62f6078904080ebab9b63
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-26 20:48:19 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,15 @@
+class Course
+  @@all = []
+  attr_accessor :title, :schedule, :description
+  def initialize
+    @@all << self
+  end
 
+  def self.all
+    @@all.dup.freeze
+  end
 
+  def self.reset_all
+    @@all.clear
+  end
+end
